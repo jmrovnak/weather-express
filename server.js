2ffa6b04bd1ee69df68dc9202c042f143d06873d
Justin Rovnak <jmrovnak@gmail.com>
2021-02-26 14:50:48 -0600

    added a stylesheet, tested

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const PORT = 5555
 
 app.set('view engine', 'ejs')
 
+app.use(express.static('public'));
+
 app.get('/', function (req, res) {
     // res.send('Server Test')
     res.render('index')
