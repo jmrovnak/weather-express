de9c462e0be75939a7358aa2968a64e176de2457
Justin Rovnak <jmrovnak@gmail.com>
2021-02-26 15:19:29 -0600

    added body parser, tested req func

--- server.js
+++ server.js
@@ -1,19 +1,21 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
 const PORT = 5555
 
 app.set('view engine', 'ejs')
-
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('public'));
 
 app.get('/', function (req, res) {
     // res.send('Server Test')
-    res.render('index')
+    res.render('index');
 })
 
-app.post('/', function (req, resp) {
+app.post('/', function (req, res) {
     res.render('index');
+    console.log(req.body.city);
 })
 
 app.listen(PORT, function () {
